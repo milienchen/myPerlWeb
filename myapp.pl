bbd51a15045fb501371e4707f87246a9c2225d01
mingchi chen <mingchi.chen@audatex.com>
2015-06-16 16:51:35 +0800

    t

---
+++ myapp.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl -w  如果是linux環境就加此參數讓os知道去哪裡載入perl.exe使用
+#perl -cw myapp.pl  檢查語法用
+#perl -w myapp.pl   若語法有誤加-w參數會顯示
+#
+#@ARGV 是內建的陣列,不必宣告就可以拿來用. 裡面存放著 command line argument 命令列上的參數
+
+print "ggla\n"x3;
+print "gglb"."ggP";
